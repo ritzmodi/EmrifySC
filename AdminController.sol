8fdcd0e76ff3a7213fdbc3529ebe51b27561efb4
vikas1188 <vikas.singh1188@gmail.com>
2017-12-12 21:57:43 +0530

    updated the contract to include ORG factor etc
    updated the contract to include ORG factor and admin group.

--- AdminController.sol
+++ AdminController.sol
@@ -10,13 +10,14 @@ contract AdminController {
     enum State { Pending, Accepted, Rejected, Terminated } 
     uint256 totalPendingCount;
     // Emrify shall add those people who can push claim against the registered Providers, Nobody Else should be able to do it    
-    mapping(address => bool ) public ListOfPeopleWhoCanPushClaim; 
+    mapping(address => bool ) public AdminGroup; 
     
     
     struct providerDetail{
     address providerAddress;
     State state;
     bool isRegistered;
+    bool isOrganization;
     string IPFSApprovalDocumentHash;
     string IPFSRemovalDocumentHash;
     // address[] sisteBranchesOfHospital;// will use later to add multiple branches of the same hospital
@@ -27,8 +28,10 @@ contract AdminController {
         _;
     }
 
+    // this condition makes sure that the operation can be done by any of the designated address
+    // they are having Admin previlleges
     function isAdmin(address addr) public returns(bool) { 
-        return addr == admin; 
+        return addr == admin ||  AdminGroup[addr] == true ; 
         
     }
 
@@ -48,14 +51,14 @@ contract AdminController {
     mapping (address => providerDetail) public WhiteListedProviders;
     
     
-    //Step 0 : add the addresses of Govt bodies or those entities who can set claim tomorrow for the providers
-    // & This can also be converted to the request/approve mechanism. even though today we are directly making a request from the Admin controller
-    function whiteListEntityWhoCanPushClaims(address _GovtBodyAddress) onlyAdmin {
-        ListOfPeopleWhoCanPushClaim[_GovtBodyAddress] = true;
+    // this group tomorrow shall handle the pressure of 
+    function addAdminGroup(address _newAdminAddress) onlyAdmin {
+        AdminGroup[_newAdminAddress] = true;
     }
     
     // Step 1:
-    function submitRequestForApproval(string _ProviderDetailsIPFShash){
+    function submitRequestForApproval(bool _isOrg, string _ProviderDetailsIPFShash){
+        WhiteListedProviders[msg.sender].isOrganization = _isOrg;
         WhiteListedProviders[msg.sender].state = State.Pending;
         WhiteListedProviders[msg.sender].providerAddress = msg.sender;
         WhiteListedProviders[msg.sender].IPFSApprovalDocumentHash = _ProviderDetailsIPFShash;
@@ -68,6 +71,8 @@ contract AdminController {
         WhiteListedProviders[_providerAddress].state = State.Accepted;
         WhiteListedProviders[_providerAddress].isRegistered = true;
         totalPendingCount--;
+        // fire an event with `isORg` variable so that we can identify that he is and org or not
+        
         
     }
     

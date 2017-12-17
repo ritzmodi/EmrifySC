99cee973313ec2183152906d4a967fbde3e88fb0
vikas1188 <vikas.singh1188@gmail.com>
2017-12-17 22:31:50 +0530

    added events in place & method level changes
    added events in place & method level changes done in the `DisassociateDoctorfromTheHospital` method

--- ProviderRegistry.sol
+++ ProviderRegistry.sol
@@ -18,6 +18,10 @@ contract ProviderRegistry is AdminController {
         
 }*/
 
+    // events
+    event DoctorAssociated(address _orgAddress, address doctorAddress, string IPFSDocumentHash);
+    event DoctorDisassociated(address _orgAddress, address doctorAddress, string IPFSDocumentHash);
+    
 function ProviderRegistry(address _adminController){
         if (_adminController != 0x0) {
             adminController = AdminController(_adminController);
@@ -36,24 +40,26 @@ function ProviderRegistry(address _adminController){
     }
     
     
-    mapping (address => DoctorDetails) public DoctorInformation;
+    mapping (address => mapping(address => DoctorDetails )) public DoctorInformation; // this mapping is to keep the relationship between the Organization and their whitelisted doctor
     
     /// this function shall be called by those hospitals which is already registered by the admin in the AdminController contract.
     function AssociateDoctorUnderMyHospital(address _doctorAddress, string _IPFSDocumentHash)  internal {
         if(WhiteListedProviders[msg.sender].isRegistered && WhiteListedProviders[msg.sender].isOrganization == true){
-            DoctorInformation[msg.sender].doctorAddress = _doctorAddress;
-            DoctorInformation[msg.sender].isRegistered = true;
-            DoctorInformation[msg.sender].IPFSDoctorApprovalDocumentHash = _IPFSDocumentHash;
+            DoctorInformation[msg.sender][_doctorAddress].doctorAddress = _doctorAddress;
+            DoctorInformation[msg.sender][_doctorAddress].isRegistered = true;
+            DoctorInformation[msg.sender][_doctorAddress].IPFSDoctorApprovalDocumentHash = _IPFSDocumentHash;
+            DoctorAssociated(msg.sender, _doctorAddress, _IPFSDocumentHash);
         } else {
             revert();//the hospital is not registered under Emrify yet.
         }
         
     }
     
-    function DisassociateDoctorfromTheHospital(){
+    function DisassociateDoctorfromTheHospital(address _doctorAddress, string _IPFSDocumentHash){
         if(WhiteListedProviders[msg.sender].isRegistered && WhiteListedProviders[msg.sender].isOrganization){
-            if(DoctorInformation[msg.sender].isRegistered == true){
-                DoctorInformation[msg.sender].isRegistered = false;
+            if(DoctorInformation[msg.sender][_doctorAddress].isRegistered == true){
+                DoctorInformation[msg.sender][_doctorAddress].isRegistered = false;
+                DoctorDisassociated(msg.sender, _doctorAddress, _IPFSDocumentHash);
             } else {
                 revert();//doctor already disassociated
             }
